a7e81a804146a13a602743e72ca8e7f6b9695bf3
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-06-23 15:38:27 -0700

    Moved writing of sin data to own function; added binaural wave function;

--- src/WavReader.c
+++ src/WavReader.c
@@ -1,11 +1,10 @@
 #include "WavReader.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-struct WAV_file create_WAV_file_sin_wave(
-		/* Endian-ness? */
-		double	 freq,
+struct WAV_file alloc_WAV_file(
 		uint16_t num_channels,
 		uint32_t sample_rate,
 		uint16_t bits_per_sample,	
@@ -46,24 +45,81 @@ struct WAV_file create_WAV_file_sin_wave(
 		},
 
 	};
-	
-    double amp = pow(10, -6.0 / 20.0) * (pow(2, bits_per_sample - 1) - 1);
-    double sample_period = 1.0 / sample_rate;
-    double ang_freq = 2.0 * M_PI * freq;
-
-    for (uint32_t i = 0; i < subchunk2_size / (bits_per_sample / 8); i++) {
-        double t = (double)i * sample_period;
-        int16_t sample = (int16_t)(amp * sin(ang_freq * t));
-
-        // For stereo, write the same sample to both left and right channels
-        for (uint16_t channel = 0; channel < num_channels; ++channel) {
-            uint32_t sample_index = i * num_channels + channel;
-            wav.data.buff[sample_index * 2] = sample & 0xFF;
-            wav.data.buff[sample_index * 2 + 1] = (sample >> 8) & 0xFF;
-        }
-    }
 
 	return wav;
+}
+
+void WAV_file_write_sin_wave(
+        struct WAV_file *wav,
+        double freq)
+{
+    // -6Db amplitude by default
+    const double amp = pow(10, -6.0 / 20.0) * (pow(2, wav->fmt.bits_per_sample - 1) - 1);
+    const double sample_period = 1.0 / wav->fmt.sample_rate;
+    const double ang_freq = 2.0 * M_PI * freq;
+    const uint16_t bytes_per_sample = (wav->fmt.bits_per_sample / 8);
+
+    // Loop for the number of samples
+    for (uint32_t sample_index = 0;
+         sample_index < wav->data.subchunk2_size /  (wav->fmt.num_channels * bytes_per_sample);
+         sample_index++) {
+
+        const double t = (double)sample_index * sample_period;
+        const int16_t sample = (int16_t)(amp * sin(ang_freq * t));
+
+        // Loop for each channel
+        for (uint16_t channel = 0; channel < wav->fmt.num_channels; ++channel) {
+            const uint32_t channel_index = sample_index * wav->fmt.num_channels + channel;
+
+            // Calculate the byte offsets based on the bit depth and channel
+            const uint32_t byte_offset = channel_index * bytes_per_sample;
+
+            // Write the sample bytes to the buffer based on the bit depth
+            for (uint16_t byte = 0; byte < bytes_per_sample; ++byte) {
+                wav->data.buff[byte_offset + byte] = (sample >> (byte * 8)) & 0xFF;
+            }
+        }
+    }
+}
+
+void WAV_file_write_biaural_wave(
+        struct WAV_file *wav,
+        double freq1,
+        double freq2)
+{
+    // -6Db amplitude by default
+    const double amp = pow(10, -6.0 / 20.0) * (pow(2, wav->fmt.bits_per_sample - 1) - 1);
+    const double sample_period = 1.0 / wav->fmt.sample_rate;
+    const double ang_freq1 = 2.0 * M_PI * freq1;
+    const double ang_freq2 = 2.0 * M_PI * freq2;
+    const uint16_t bytes_per_sample = (wav->fmt.bits_per_sample / 8);
+
+    // Loop for the number of samples
+    for (uint32_t sample_index = 0;
+         sample_index < wav->data.subchunk2_size /  (wav->fmt.num_channels * bytes_per_sample);
+         sample_index++) {
+
+        const double t = (double)sample_index * sample_period;
+        const int16_t sample1 = (int16_t)(amp * sin(ang_freq1 * t));
+        const int16_t sample2 = (int16_t)(amp * sin(ang_freq2 * t));
+
+        // Loop for each channel
+        for (uint16_t channel = 0; channel < wav->fmt.num_channels; ++channel) {
+            const uint32_t channel_index = sample_index * wav->fmt.num_channels + channel;
+
+            // Calculate the byte offsets based on the bit depth and channel
+            const uint32_t byte_offset = channel_index * bytes_per_sample;
+
+            // Write the sample bytes to the buffer based on the bit depth
+            for (uint16_t byte = 0; byte < bytes_per_sample; ++byte) {
+                if (channel % 2 == 0) {
+                    wav->data.buff[byte_offset + byte] = (sample1 >> (byte * 8)) & 0xFF;
+                } else {
+                    wav->data.buff[byte_offset + byte] = (sample2 >> (byte * 8)) & 0xFF;
+                }
+            }
+        }
+    }
 }
 
 void free_WAV_file(struct WAV_file file) {
@@ -71,66 +127,63 @@ void free_WAV_file(struct WAV_file file) {
 	free(file.data.buff);
 }
 
-FILE* write_WAV_to_file(struct WAV_file wav, const char* file_name)
+void write_WAV_to_file(
+        struct WAV_file* wav,
+        const char* file_name)
 {
-	FILE* ret_file = fopen(file_name, "w");
+	FILE* file = fopen(file_name, "w");
 
-	if (ret_file == NULL) {
+	if (file == NULL) {
 		perror("File opening failed\n");
-		fclose(ret_file);
-		return NULL;
+		fclose(file);
 	}
 
 	size_t riff_ret = fwrite(
-			&wav.riff,
-			sizeof(wav.riff),
+			&wav->riff,
+			sizeof(wav->riff),
 			1,
-			ret_file
+			file
 		);
 
 	if (riff_ret != 1) {
 		perror("Failed to write RIFF chunk\n");
-		fclose(ret_file);
-		return NULL;
+		fclose(file);
 	}
 
 	size_t fmt_ret = fwrite(
-			&wav.fmt,
-			sizeof(wav.fmt),
+			&wav->fmt,
+			sizeof(wav->fmt),
 			1,
-			ret_file
+			file
 		);
 
 	if (fmt_ret != 1) {
 		perror("Failed to write FMT chunk\n");
-		fclose(ret_file);
-		return NULL;
+		fclose(file);
 	}
 
 	size_t data_ret = fwrite(
-			&wav.data,
-			sizeof(wav.data) - sizeof(wav.data.buff),
+			&wav->data,
+			sizeof(wav->data) - sizeof(wav->data.buff),
 			1,
-			ret_file
+			file
 		);
 	
 	if (data_ret != 1) {
 		perror("Failed to write DATA chunk\n");
-		fclose(ret_file);
-		return NULL;
+		fclose(file);
 	}
 
 	size_t sound_data_ret = fwrite(
-			wav.data.buff,
+			wav->data.buff,
 			sizeof(unsigned char),
-			wav.data.subchunk2_size,
-			ret_file);
+			wav->data.subchunk2_size,
+			file
+        );
 
-	if (sound_data_ret != wav.data.subchunk2_size) {
+	if (sound_data_ret != wav->data.subchunk2_size) {
 		perror("Failed to write sound data\n");
-		fclose(ret_file);
-		return NULL;
 	}
 
-	return ret_file;
+    fclose(file);
 }
