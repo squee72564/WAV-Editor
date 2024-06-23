62834df3e6288acf6e6a17c7b4816fe61a3fec9d
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-06-23 15:37:17 -0700

    Minor edits to comments; added definition for write sin/binaurual wave

--- include/WavReader.h
+++ include/WavReader.h
@@ -2,19 +2,18 @@
 #define WAV_READER_C_H 
 
 #include <stdint.h>
-#include <stdio.h>
 
 // Source : https://ccrma.stanford.edu/courses/422-winter-2014/projects/WaveFormat/
 
 struct RIFF_chunk {
-	unsigned char 	chunk_id[4];		// contains letters "RIFF" in ascii for little-endian data files, "RIFX" for big-endian data files
+	unsigned char 	chunk_id[4];		// ascii letters "RIFF" for little-endian, "RIFX" for big-endian
 	uint32_t 	chunk_size;		// 36 + subchunk2_size
-	unsigned char 	format[4];		// contains letters WAVE in ascii
+	unsigned char 	format[4];		// ascii letters "WAVE"
 };
 
 struct FMT_chunk {
-	unsigned char 	subchunk1_id[4];	// contains letters "fmt" in ascii
-	uint32_t 	subchunk1_size;		// size of rest of subchunk following this field, should be 16 for PCM
+	unsigned char 	subchunk1_id[4];	// ascii letters "fmt"
+	uint32_t 	subchunk1_size;		// size of rest of subchunk following this field; 16 for PCM
 	uint16_t  	audio_format;		// PCM = 1, other number indicates some form of compression
 	uint16_t 	num_channels;		// 1 = mono, 2 = stereo, etc.
 	uint32_t	sample_rate;		// 8000, 44100, 48000, etc.
@@ -25,8 +24,8 @@ struct FMT_chunk {
 };
 
 struct DATA_chunk {
-	unsigned char 	subchunk2_id[4];	// contains letters "data"
-	uint32_t	subchunk2_size;		// Number of bytes in data; size of rest of subchunk after this field
+	unsigned char 	subchunk2_id[4];	// ascii letters "data"
+	uint32_t	subchunk2_size;		// Number of bytes in sound data
 	unsigned char* 	buff;			// actual sound data
 };
 
@@ -38,18 +37,29 @@ struct WAV_file {
 	struct DATA_chunk data;
 };
 
-
-struct WAV_file create_WAV_file_sin_wave(
-		/* Endian-ness? */
-		double	 frequency,
+struct WAV_file alloc_WAV_file(
 		uint16_t num_channels,
 		uint32_t sample_rate,
 		uint16_t bits_per_sample,	
 		uint32_t duration /* seconds */
 	);
 
+void WAV_file_write_sin_wave(
+        struct WAV_file *file,
+        double frequency
+    );
+
+void WAV_file_write_biaural_wave(
+        struct WAV_file *file,
+        double frequency1,
+        double frequency2
+    );
+
 void free_WAV_file(struct WAV_file file);
 
-FILE* write_WAV_to_file(struct WAV_file wav, const char* file_name);
+void write_WAV_to_file(
+        struct WAV_file *wav,
+        const char* file_name
+    );
 
 #endif
