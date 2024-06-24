10495f1f6c119594c310e9c4b323d0d0c3552337
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-06-23 19:17:13 -0700

    Edited functions to match definitions

--- tests/create_sin_wave.c
+++ tests/create_sin_wave.c
@@ -5,19 +5,18 @@ int main(void) {
 	struct WAV_file wav = alloc_WAV_file(
 			2,	// channels
 			44100,	// sample rate
-			16,	// bits per sample
-			20	// duration (seconds)
+			16	// bits per sample
 		);
 
-    WAV_file_write_sin_wave(&wav, 174.0);
+    WAV_file_write_sin_wave(&wav, 174.0, 20);
 
     write_WAV_to_file(&wav, "test-sin.wav");
 
-    WAV_file_write_binaural_wave(&wav, 174.0, 164.0);
+    WAV_file_write_binaural_wave(&wav, 174.0, 164.0, 20);
     
     write_WAV_to_file(&wav, "test-binaural.wav");
 
-    free_WAV_file(wav);
+    free_WAV_file(&wav);
 
 	return 0;
 }
