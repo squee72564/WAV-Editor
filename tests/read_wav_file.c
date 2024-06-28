def6feb28c7ddd8f5cb8db16776a2f8cde31a942
arod183 <rodriguez.al183@gmail.com>
2024-06-28 12:58:13 -0700

    Add test for reading wav file

---
+++ tests/read_wav_file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "WavReader.h"
+
+int main(void) {
+
+	read_WAV_file("/home/alan/Music/My Records/[Kimochi]/[Kimochi 23]  Area - Anhedral/A1 For HN.wav");
+
+	return 0;
+}
