3f552ff7c94fe357d9bde13edb897545582dc289
squee72564 <rodriguez.al183@gmail.com>
2024-07-08 11:48:38 -0700

    Edit test function for reading and printing file

--- tests/read_wav_file.c
+++ tests/read_wav_file.c
@@ -2,21 +2,26 @@
 #include <stdio.h>
 #include "WavReader.h"
 
-int main(void) {
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <wav file path>\n", argv[0]);
+		return 1;
+	}
 
 	struct WAV_file wav;
 	memset(&wav, 0, sizeof(wav));
 
-	int ret = read_WAV_file(
-			&wav,
-			"/home/alan/Music/My Records/[Kimochi]/[Kimochi 23]  Area - Anhedral/A1 For HN.wav"
-		);
-
-	if (ret == 1) {
-		print_WAV_file(&wav);
-	} else {
-		perror("Could not read wav file\n");
+	if (!read_WAV_file(&wav, argv[1])) {
+		perror("Error: Could not read wav file!\n");
+		return 1;
 	}
+
+	print_WAV_file(&wav);
+
+	print_WAV_file_db(&wav);
+
+	free_WAV_file(&wav);
 
 	return 0;
 }
