168f1051247497b6fc2d2a35a2600a1581cfb3d6
squee72564 <rodriguez.al183@gmail.com>
2024-07-15 16:59:30 -0700

    Minor edit

--- tests/read_wav_file.c
+++ tests/read_wav_file.c
@@ -12,13 +12,13 @@ int main(int argc, char** argv)
 	struct WAV_file wav;
 	memset(&wav, 0, sizeof(wav));
 
+	printf("\nReading wav file: %s\n\n", argv[1]);
+
 	// Read wav file into struct
 	if (WAV_read_file(&wav, argv[1]) == Error) {
 		perror("Error: Could not read wav file!\n");
 		return 1;
 	}
-
-	printf("\nReading wav file: %s\n\n", argv[1]);
 	
 	WAV_print(&wav);
 
